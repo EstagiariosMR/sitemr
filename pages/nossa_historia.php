3a35468b30c4b98c72dc1418acf8bf4d37de633a
João Roberto <jr.teixeira2010@gmail.com>
2025-06-12 18:57:10 -0300

    commit novas páginas - miguel

---
+++ pages/nossa_historia.php
@@ -0,0 +1,172 @@
+<main>
+
+<h1>História completa</h1>
+
+<h2>Dados bibliográficos da professora Maria Manuela Rocha</h2>
+
+<ul>
+
+ <li><strong>Nome:</strong> Maria Manuela Rocha;</li>
+ <li><strong>Data de nascimento:</strong> 23 de novembro de 1904;</li>
+ <li><strong>Data de falescimento:</strong> 1° de outubro de 1958;</li>
+ <li><strong>Filiação:</strong> Manoel Marques da Rocha e Bertholina Junqueira Rocha.</li>
+
+</ul>
+
+<p>Maria Rocha foi à última filha de uma família de 14 (quatorze) filhos. Sua mãe era professora, e foi com ela que aprendeu as primeiras letras, isto é, lhe ministrou os conhecimentos do Curso Primário. Mais tarde vai interna para o Colégio dos Anjos, em Porto Alegre, para fazer o Curso Complementar. Em 1925, recebeu o diploma de professora.</p>
+
+<p>Logo depois de formada, começou trabalhar no Colégio Elementar, depois Escola Normal "Olavo Bilac". Foi a primeira Assistente de Direção da professora Emília Flores, na Escola Normal e respondeu pela direção do estabelecimento.</p>
+
+<p>Idealista que era, um dia visitou o Presídio Municipal e viu que lá, muito poderia fazer a favor daquelas criaturas. Assim pensando, fundou em 27 de maio de 1955, a Escolinha São Francisco de Assis, que tinha como objetivo alfabetizar e recuperar os presidiários, através de uma terapêutica ocupacional.</p>
+
+<p>Foi uma das principais figuras da comissão organizadora dos festejos do Centenário de Santa Maria, sendo idealizadora de uma festa popular, "A Chuva de Estrelas".</p>
+
+<p>Em 1º de outubro de 1958, às 3 horas e 45 minutos, faleceu em sua residência, vítima de câncer.</p>
+
+<p>Seu corpo foi velado no auditório da Escola Normal "Olavo Bilac". Em ato tocante, a Escola do Presídio Municipal foi levar o derradeiro adeus à sua amada benfeitora.</p>
+
+<p>Sua morte abalou não só o magistério, como também o mundo intelectual e a sociedade Santamarienses. De imediato surgiu entre seus colegas a ideia de dar ao Ginásio da Escola o nome da incomparável mestra, atendendo a um dever da justiça.</p>
+
+<p>Em 28 de dezembro de 1962 nossa Escola passou a denominar-se Colégio Estadual Profª Maria Rocha, hoje E.E.E.M.P. Maria Rocha.</p>
+
+<p>Maria Manuela Rocha está entre nós e estará sempre através da história e de seu lema: “Ama teu próximo mais que a ti mesmo”.</p>
+
+<h2>Histórico e Atos Oficiais</h2>
+
+<p><strong>1)</strong> Decreto-lei de criação n.º 97, de 16 de julho de 1941, com a denominação de Curso Ginasial, anexo à Escola Normal "Olavo Bilac", publicado no Diário Oficial de 17.04.1941.</p>
+
+<p><strong>2)</strong> Decreto n.º 7654 de 08 de fevereiro de 1957, publicado no Diário Oficial de 12.02.1957, que o desanexa da Escola Normal "Olavo Bilac".</p>
+
+<p><strong>3)</strong> Decreto n.º 9365 de 19 de setembro de 1958, publicado no Diário Oficial de 19.09.1958, que passa a ser denominado Ginásio Estadual Caetano Pagliuca.</p>
+
+<p><strong>4)</strong> Decreto n.º 14.010, de 22 de agosto de 1962, publicado no Diário Oficial de 25.08.1962, transformando-o em Colégio Estadual Caetano Pagliuca.</p>
+
+<p><strong>5)</strong> Decreto 14.639 de 28 de dezembro de 1962, passa a ser denominado Colégio Estadual Maria Rocha (consta no Parecer 12/12/80 do Conselho Estadual de Educação).</p>
+
+<p><strong>6)</strong> Decreto n.º 14.768, de 21 de janeiro de 1963, publicado no Diário Oficial de 22.01.1963 alterando o nome para Colégio Estadual Professora Maria Rocha.</p>
+
+<p><strong>7)</strong> Portaria n.º 30.423 de 10 de dezembro de 1979, publicada no Diário Oficial de 14.12.1979, passando a denominar-se Escola Estadual de II Grau Professora Maria Rocha.</p>
+
+<p><strong>8)</strong> Portaria ATO/SE, n.º 00111, de 19 de abril de 2000- de Escola Estadual de 2° Grau Professora Maria Rocha para Escola Estadual de Ensino Médio Professora Maria Rocha.</p>
+
+<p><strong>9)</strong> Curso Técnico de Contabilidade, Subsequente - Parecer n° 934/98 de 14 de outubro de 1998.</p>
+
+<p><strong>10)</strong> Curso Técnico em Secretariado, Subsequente - Parecer n.º 362/99 de 28 de abril de 1999.</p>
+
+<p><strong>11)</strong> Curso Técnico em Informática, Subsequente - Parecer n° 362/99 de 28 de abril de 1999.</p>
+
+<p><strong>12)</strong> 2014 – Aprovação do Curso Técnico de Informática Integrado ao Ensino Médio.</p>
+
+<h2>Diretores</h2>
+
+<p> A Escola Estadual de Ensino Médio Professora Maria Rocha teve 14 diretores em comum com o Instituto de Educação Olavo Bilac. E, depois que as direções dividem-se, teve 22 diretores. Ao todo são 36 diretores.</p>
+
+<p>Professores que exerceram a direção da Escola desde a sua criação:</p>
+
+<p><strong>1)</strong> Direções em conjunto: Escola Maria Rocha com Olavo Bilac – de 1941 a abril de 1963. Em abril de 1963 as direções dividem-se e assume no Maria Rocha a professora Diva Muller da Rocha.</p>
+
+<ul>
+    <li>Alda Saldanha – 1941 a 1945</li>
+     
+    <li>Talita Alcides Brito – Responsável pela direção – 08/04/1946 a 28/04/1946</li>
+ 
+    <li>Maria de Lourdes Andrade Galant – 19/04/1946 a 1947</li>
+
+    <li>Ilka Nessi – 11/1947 a 1951</li>
+
+    <li>Emilia Flores – 03/03/1951 a 1952</li>
+
+    <li>Maria Manuela Rocha – responsável pela direção 12/03/1952 a 01/08/1952</li>
+
+    <li>Maria Luiza Roth – 08/1952 a 1953</li>
+
+    <li>Catharina Colussi – responsável pela direção – 09/1953 a 03/1954</li>
+
+    <li>Artheniza Weimann Rocha – 01/04/1954 a 1955</li>
+
+    <li>Agueda Brazzale Leal – 21/02/1955 a 1959</li> 
+ 
+   
+</ul>
+
+ <p><strong>OBS:</strong> Em sua gestão, houve o processo de desanexação, em 08/02/1957, do ginásio da Escola Normal Olavo Bilac.</p>
+
+<ul>
+
+   <li>Garibaldi Poggetti – responsável pela direção de 21 a 27/02/1959</li>
+
+   <li>Artheniza Weimann Rocha - 28/02/1959 a 1961</li>
+
+   <li>Adelmo Simas Genro – 14/03/1961 a 1962</li>
+
+   <li>Olavo Antonio Sarzi Sartori responsável pela direção de 07/11/1962 a 06/03/1963</li>
+
+</ul>
+
+<p><strong>OBS:</strong>  Em sua gestão, a Escola transformou-se em COLÉGIO e tem, pela primeira vez, a denominação de “Maria Rocha” – Colégio Estadual Maria Rocha (28/12/1962). Posteriormente, Colégio Estadual Professora Maria Rocha (21/01/1963).</p>
+
+<ul>
+   <li>Maria Luzel de O. Cauduro – responsável pela direção 07/03/1963 a 16/03/1963</li>
+   
+   <li>Agueda Brazzale Leal – 16/03/1963 a 04/1963</li>
+
+</ul>
+
+<p><strong>2)</strong> Dividem-se as direções, não mais as direções são as mesmas para o Olavo Bilac e Maria Rocha.</p>
+
+<ul>
+
+   <li>Diva Muller da Rocha – 09/04/1963 a 1964</li>
+
+   <li>Leonidas M. Didonet – 1964 a 1965</li>
+
+   <li>Clecy Abreu Appel – 1965 a 1966</li>
+    
+   <li>Darcila de La Canal Castelan – 1966 a 1972</li>
+
+   <li>Darcy Mendes de Oliveira – responsável pela direção – 06/1972 a 08/1972</li>
+
+   <li>Francisco Xavier Pillar – 1972 a 1973</li>
+
+   <li>Eda Motta – responsável pela direção – 10/1973 a 11/1973</li>
+
+   <li>Haydée Neves – responsável pela direção 12/1973 a 08/1974</li>
+
+   <li>Glecy Romera Borin – de 1974 a 1980</li>
+
+   <li>Rony Sergio Carnieletto – 1980 a 1985</li>
+
+   <li>Terezinha B. Espindola – 01/1985 a 04/1985</li>
+
+   <li>Ariete T. Schanadelbach e Silva – 1985 a 1991</li>
+
+   <li>Tania Maria Ferreria Valente – 12/1991 a /02/1994</li>
+
+   <li>Maria Rovanda Ferro – 03/1994 a 01/1995</li>
+
+   <li>Sonia Fonseca Hoehr – 01/1995 a 28/12/1995</li>
+
+  <li>Maria Rovanda Ferro – 29/12/1995 a 30/12/1999</li>
+
+  <li>Lírio Demarco – 2000 a 2003</li>
+
+  <li>Marli Onorina da Silveira – 2004 a 2006</li>
+
+  <li>Lírio Demarco – 2007 a 2012</li>
+ 
+ <li>Cleunice Dornelles Fialho – 2012 a 2017</li>
+
+ <li>Ivelise Mostardeiro Borges Pereira – 03/04/2018 a 31/12/2021</li>
+
+  <li>Isa Cristina Barbosa Pereira - desde 2022</li>
+</ul>
+
+<p>A Escola Estadual de Ensino Médio Professora Maria Rocha enriquece a cada dia a sua história em prol da educação e enobrece e contribui para a cultura de nossa cidade há mais de 80 anos. Tem apoiado significativamente a formação integral dos jovens, a criatividade, a autonomia e a pesquisa, através de uma prática educativa atualizada e assumida pelos profissionais que atuam e pelos que já atuaram nessa nobre instituição de ensino.</p>
+
+<p>A Escola, graças ao dinamismo, dedicação e interesse das pessoas que nela trabalham e ao êxito dos nossos alunos nos bancos universitários e no campo profissional se projeta no cenário cultural de nossa cidade.</p>
+
+<p>A Escola busca além de excelência acadêmica, a formação dos jovens em todas as dimensões ética, espiritual, física, afetiva, valorativa, além de possibilitar aos alunos, a integração ao mundo contemporâneo nos aspectos fundamentais da cidadania e do trabalho.</p>
+
+<p>A Escola, muito atenta às necessidades da comunidade no que diz respeito ao mercado de trabalho oferta, também, o Ensino Profissionalizante, em nível Pós-Médio com os cursos de Contabilidade, Informática e Secretariado. Oportuniza aos alunos a ampliação de oportunidades de atuação na vida moderna, preparando-os para o mundo tecnológico e científico, integrando-os ao trabalho e proporcionando o desenvolvimento individual e interpessoal, aproximando a Escola do mundo real. Assim, há empenho de toda a comunidade escolar no sentido de formar jovens preparados pra atuar profissionalmente, na sociedade com competência e qualidade.</p>
+
+</main>
--- pages/quem_somos.php
+++ pages/quem_somos.php
@@ -1,3 +1,33 @@
 <main>
-    <h1>Quem Somos</h1>
+
+<h1>Quem Somo?</h1>
+
+<p>A Escola Estadual de Ensino Médio Professora Maria Rocha enriquece a cada dia a sua história em prol da educação. Enobrece e contribui para a cultura de nossa cidade há mais de 80 anos.</p>
+
+<p> Tem apoiado significativamente a formação integral dos jovens. A criatividade, autonomia e a pesquisa, através de uma prática educativa atualizada e assumida pelos profissionais que atuam, e pelos que já atuaram nessa nobre instituição de ensino. </p>
+
+<p> A Escola, graças ao dinamismo, dedicação e interesse das pessoas que nela trabalham e ao êxito dos nossos alunos nos bancos universitários e no campo profissional, se projeta no cenário cultural de nossa cidade.</p>
+
+<p>img</p>
+
+<p> A Escola busca além de excelência acadêmica, a formação dos jovens em todas as dimensões ética, espiritual, física, afetiva, valorativa, além de possibilitar aos alunos, a integração ao mundo contemporâneo nos aspectos fundamentais da cidadania e do trabalho. </p>
+
+<p>A Escola, muito atenta às necessidades da comunidade no que diz respeito ao mercado de trabalho oferta, também, o Ensino Profissionalizante - em nível Pós-Médio - com os cursos técnicos em Contabilidade, Informática e Secretariado. Oportuniza aos alunos a ampliação de oportunidades de atuação na vida moderna, preparando-os para o mundo tecnológico e científico, integrando-os ao trabalho e proporcionando o desenvolvimento individual e interpessoal, aproximando a Escola do mundo real. Assim, há empenho de toda a comunidade escolar no sentido de formar jovens preparados pra atuar profissionalmente, na sociedade com competência e qualidade. </p>
+
+<p>img</p>
+
+<p>A Escola Estadual de Ensino Médio Professora Maria Rocha completou, em 2021, 80 anos de atividades educacionais. Está situada na rua Conde de Porto Alegre, nº 795, na cidade de Santa Maria.</p>
+
+<h1>Nossa escola funciona nos três turnos - manhã, tarde e noite - e possui três modalidades de ensino:</h1>
+<ul>
+    <li>Ensino Médio;</li>
+    <li>Curso Técnico em Informática Integrado ao Ensino Médio;</li>
+    <li>Cursos Técnicos Subsequentes em Contabilidade, Informática e Secretariado.</li>
+
+</ul>
+
+<a href="nossa_historia">
+<Button>História completa</Button>
+</a>
+
 </main>
