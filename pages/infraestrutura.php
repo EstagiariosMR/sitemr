59cf8bdd45c9f289c361cdc48801d2b5eaf10975
João Roberto <jr.teixeira2010@gmail.com>
2025-06-27 19:43:33 -0300

    novo grid layout infraestrutura

--- pages/infraestrutura.php
+++ pages/infraestrutura.php
@@ -1,7 +1,7 @@
 <main>
     <h1>Infraestrutura</h1>
 
-    	<section id="icones">
+    <section id="icones">
 		<h2 class="bi bi-bookmarks" style="color: #354698; margin: 0 5% 0 5%;"> Infraestrutura</h2>
 	</section>
 
@@ -9,153 +9,117 @@
 
 	<section id="infraestrutura" class="infraestrutura">
 
-		<div class="row row-cols-2 row-cols-md-6 g-2">
-			<div class="col">
-				<div class="" style="border: none;">
-					<div align="center"><img src="https://mariarocha.org.br/site/assets/icones/dependencias-com-acessibilidade.png" class="card-img-top" style="margin-bottom: 10px;"></div>
-					<div class="card-body">
-						<p class="card-title" style="text-align: center;">Dependências com acessibilidade</p>
-					</div>
-				</div>
-			</div>
-			<div class="col">
-				<div class="" style="border: none;">
-					<div align="center"><img src="https://mariarocha.org.br/site/assets/icones/banheiros-com-acessibilidade.png" class="card-img-top" style="margin-bottom: 10px;"></div>
-					<div class="card-body">
-						<p class="card-title" style="text-align: center;">Banheiros com acessibilidade</p>
-					</div>
-				</div>
-			</div>
-			<div class="col">
-				<div class="" style="border: none;">
-					<div align="center"><img src="https://mariarocha.org.br/site/assets/icones/orientacao-pedagogica.png" class="card-img-top" style="margin-bottom: 10px;"></div>
-					<div class="card-body">
-						<p class="card-title" style="text-align: center;">Orientação pedagógica</p>
-					</div>
-				</div>
-			</div>
-			<div class="col">
-				<div class="" style="border: none;">
-					<div align="center"><img src="https://mariarocha.org.br/site/assets/icones/cozinha.png" class="card-img-top" style="margin-bottom: 10px;"></div>
-					<div class="card-body">
-						<p class="card-title" style="text-align: center;">Cozinha</p>
-					</div>
-				</div>
-			</div>
-			<div class="col">
-				<div class="" style="border: none;">
-					<div align="center"><img src="https://mariarocha.org.br/site/assets/icones/refeitorios.png" class="card-img-top" style="margin-bottom: 10px;"></div>
-					<div class="card-body">
-						<p class="card-title" style="text-align: center;">Refeitórios</p>
-					</div>
-				</div>
-			</div>
-			<div class="col">
-				<div class="" style="border: none;">
-					<div align="center"><img src="https://mariarocha.org.br/site/assets/icones/wifi.png" class="card-img-top" style="margin-bottom: 10px;"></div>
-					<div class="card-body">
-						<p class="card-title" style="text-align: center;">Wi-Fi</p>
-					</div>
-				</div>
-			</div>
-			<div class="col">
-				<div class="" style="border: none;">
-					<div align="center"><img src="https://mariarocha.org.br/site/assets/icones/agua-filtrada.png" class="card-img-top" style="margin-bottom: 10px;"></div>
-					<div class="card-body">
-						<p class="card-title" style="text-align: center;">Água filtrada</p>
-					</div>
-				</div>
-			</div>
-			<div class="col">
-				<div class="" style="border: none;">
-					<div align="center"><img src="https://mariarocha.org.br/site/assets/icones/biblioteca.png" class="card-img-top" style="margin-bottom: 10px;"></div>
-					<div class="card-body">
-						<p class="card-title" style="text-align: center;">Biblioteca</p>
-					</div>
-				</div>
-			</div>
-			<div class="col">
-				<div class="" style="border: none;">
-					<div align="center"><img src="https://mariarocha.org.br/site/assets/icones/sala-de-multimidia.png" class="card-img-top" style="margin-bottom: 10px;"></div>
-					<div class="card-body">
-						<p class="card-title" style="text-align: center;">Salas de multimídia</p>
-					</div>
-				</div>
-			</div>
-			<div class="col">
-				<div class="" style="border: none;">
-					<div align="center"><img src="https://mariarocha.org.br/site/assets/icones/sala-de-professores.png" class="card-img-top" style="margin-bottom: 10px;"></div>
-					<div class="card-body">
-						<p class="card-title" style="text-align: center;">Sala de professores</p>
-					</div>
-				</div>
-			</div>
-			<div class="col">
-				<div class="" style="border: none;">
-					<div align="center"><img src="https://mariarocha.org.br/site/assets/icones/quadra.png" class="card-img-top" style="margin-bottom: 10px;"></div>
-					<div class="card-body">
-						<p class="card-title" style="text-align: center;">Quadra de esportes</p>
-					</div>
-				</div>
-			</div>
-			<div class="col">
-				<div class="" style="border: none;">
-					<div align="center"><img src="https://mariarocha.org.br/site/assets/icones/sala-de-artes.png" class="card-img-top" style="margin-bottom: 10px;"></div>
-					<div class="card-body">
-						<p class="card-title" style="text-align: center;">Sala de artes</p>
-					</div>
-				</div>
-			</div>
-			<div class="col">
-				<div class="" style="border: none;">
-					<div align="center"><img src="https://mariarocha.org.br/site/assets/icones/lab-quimica.png" class="card-img-top" style="margin-bottom: 10px;"></div>
-					<div class="card-body">
-						<p class="card-title" style="text-align: center;">Laboratório de química</p>
-					</div>
-				</div>
-			</div>
-			<div class="col">
-				<div class="" style="border: none;">
-					<div align="center"><img src="https://mariarocha.org.br/site/assets/icones/lab-biologia.png" class="card-img-top" style="margin-bottom: 10px;"></div>
-					<div class="card-body">
-						<p class="card-title" style="text-align: center;">Laboratório de biologia</p>
-					</div>
-				</div>
-			</div>
-			<div class="col">
-				<div class="" style="border: none;">
-					<div align="center"><img src="https://mariarocha.org.br/site/assets/icones/auditorios.png" class="card-img-top" style="margin-bottom: 10px;"></div>
-					<div class="card-body">
-						<p class="card-title" style="text-align: center;">Auditórios</p>
-					</div>
-				</div>
-			</div>
-			<div class="col">
-				<div class="" style="border: none;">
-					<div align="center"><img src="https://mariarocha.org.br/site/assets/icones/lab-informatica.png" class="card-img-top" style="margin-bottom: 10px;"></div>
-					<div class="card-body">
-						<p class="card-title" style="text-align: center;">Laboratórios de Informática</p>
-					</div>
-				</div>
-			</div>
-			<div class="col">
-				<div class="" style="border: none;">
-					<div align="center"><img src="https://mariarocha.org.br/site/assets/icones/lab-contabilidade.png" class="card-img-top" style="margin-bottom: 10px;"></div>
-					<div class="card-body">
-						<p class="card-title" style="text-align: center;">Laboratórios de Contabilidade</p>
-					</div>
-				</div>
-			</div>
-			<div class="col">
-				<div class="" style="border: none;">
-					<div align="center"><img src="https://mariarocha.org.br/site/assets/icones/lab-secretariado.png" class="card-img-top" style="margin-bottom: 10px;"></div>
-					<div class="card-body">
-						<p class="card-title" style="text-align: center;">Laboratórios de Secretariado</p>
-					</div>
-				</div>
-			</div>
-		</div>
-	</section>
+		<div class="image-grid">
+        	<div class="card">
+            	<img src="imagem1.jpg" alt="Imagem 1">
+            	<div class="card-body">
+                	<p class="card-title">Dependências com acessibilidade</p>
+            	</div>
+        	</div>
+        	<div class="card">
+            	<img src="imagem2.jpg" alt="Imagem 2">
+            	<div class="card-body">
+                	<p class="card-title">Imagem 2 com descrição</p>
+            	</div>
+        	</div>
+        	<div class="card">
+            	<img src="imagem2.jpg" alt="Imagem 2">
+            	<div class="card-body">
+                	<p class="card-title">Imagem 2 com descrição</p>
+            	</div>
+        	</div>
+			<div class="card">
+            	<img src="imagem2.jpg" alt="Imagem 2">
+            	<div class="card-body">
+                	<p class="card-title">Imagem 2 com descrição</p>
+            	</div>
+        	</div>
+			<div class="card">
+            	<img src="imagem2.jpg" alt="Imagem 2">
+            	<div class="card-body">
+                	<p class="card-title">Imagem 2 com descrição</p>
+            	</div>
+        	</div>
+			<div class="card">
+            	<img src="imagem2.jpg" alt="Imagem 2">
+            	<div class="card-body">
+                	<p class="card-title">Imagem 2 com descrição</p>
+            	</div>
+        	</div>
+			<div class="card">
+            	<img src="imagem2.jpg" alt="Imagem 2">
+            	<div class="card-body">
+                	<p class="card-title">Imagem 2 com descrição</p>
+            	</div>
+        	</div>
+			<div class="card">
+            	<img src="imagem2.jpg" alt="Imagem 2">
+            	<div class="card-body">
+                	<p class="card-title">Imagem 2 com descrição</p>
+            	</div>
+        	</div>
+			<div class="card">
+            	<img src="imagem2.jpg" alt="Imagem 2">
+            	<div class="card-body">
+                	<p class="card-title">Imagem 2 com descrição</p>
+            	</div>
+        	</div>
+			<div class="card">
+            	<img src="imagem2.jpg" alt="Imagem 2">
+            	<div class="card-body">
+                	<p class="card-title">Imagem 2 com descrição</p>
+            	</div>
+        	</div>
+			<div class="card">
+            	<img src="imagem2.jpg" alt="Imagem 2">
+            	<div class="card-body">
+                	<p class="card-title">Imagem 2 com descrição</p>
+            	</div>
+        	</div>
+			<div class="card">
+            	<img src="imagem2.jpg" alt="Imagem 2">
+            	<div class="card-body">
+                	<p class="card-title">Imagem 2 com descrição</p>
+            	</div>
+        	</div>
+			<div class="card">
+            	<img src="imagem2.jpg" alt="Imagem 2">
+            	<div class="card-body">
+                	<p class="card-title">Imagem 2 com descrição</p>
+            	</div>
+        	</div>
+			<div class="card">
+            	<img src="imagem2.jpg" alt="Imagem 2">
+            	<div class="card-body">
+                	<p class="card-title">Imagem 2 com descrição</p>
+            	</div>
+        	</div>
+			<div class="card">
+            	<img src="imagem2.jpg" alt="Imagem 2">
+            	<div class="card-body">
+                	<p class="card-title">Imagem 2 com descrição</p>
+            	</div>
+        	</div>
+			<div class="card">
+            	<img src="imagem2.jpg" alt="Imagem 2">
+            	<div class="card-body">
+                	<p class="card-title">Imagem 2 com descrição</p>
+            	</div>
+        	</div>
+			<div class="card">
+            	<img src="imagem2.jpg" alt="Imagem 2">
+            	<div class="card-body">
+                	<p class="card-title">Imagem 2 com descrição</p>
+            	</div>
+        	</div>
+			<div class="card">
+            	<img src="imagem2.jpg" alt="Imagem 2">
+            	<div class="card-body">
+                	<p class="card-title">Imagem 2 com descrição</p>
+            	</div>
+        	</div>
+    	</div>
 
+	</section>
 
 </main>
